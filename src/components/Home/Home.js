e876e32cd3e1d0491b10bf564b4d9a3974866447
rlramire10 <77511166+rlramire10@users.noreply.github.com>
2021-02-18 18:20:34 -0600

    Add files via upload
    I updated the buttons used in the Main Menu of the site. They are still currently seen by all employee levels.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,21 +6,36 @@ import {Link} from 'react-router-dom'
 const Home = () => {
 
     return(
+	//Edited By Rudy Ramirez 2/18/21
+	//Button disabled variant = unclickable button
+	//Button variant = clickable buttton
+	//I renamed the testing button to Create Employee since that was the function of the button.
+	//Task 5 did not request the feature Employee Details Button but since it is a smart inclusion I am going to leave it alone.
+	//I added a Sales Report Button and kept it unclickable.
         <div className={"mainMenu"}>
-            <Button disabled variant="contained" color="primary">
+            
+			<Button disabled variant="contained" color="primary">
                 Start Transaction
             </Button>
+
             <Button variant="contained" color="primary" component={Link} to={"/shop"}>
                 Product List
+            </Button>            
+			
+			<Button variant="contained" color="primary" component={Link} to={"/test"}>
+                Create Employee
             </Button>
-            <Button variant="contained" color="primary" component={Link} to={"/employd"}>
+			
+			<Button variant="contained" color="primary" component={Link} to={"/employd"}>
                 Employee Details
             </Button>
-            <Button disabled variant="contained" color="primary">
-                Cashier Report
+			
+			<Button disabled variant="contained" color="primary">
+                Sales Report
             </Button>
-            <Button variant="contained" color="primary" component={Link} to={"/test"}>
-                Testing
+			
+			<Button disabled variant="contained" color="primary">
+                Cashier Report
             </Button>
         </div>
     )
